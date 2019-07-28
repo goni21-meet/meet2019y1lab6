c75fab3b544a13d1ad8bef13e5675ff5ea165a0d
goni21-meet <goni21@meet.mit.edu>
2019-07-28 09:32:22 -0400

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,10 @@
+
+def add_num():
+    total = 0
+    for number in range (1, 10 + 1):
+        print (number)
+        total = total + number
+    return total
+#print(total)
+answer = add_num()
+print (answer)
